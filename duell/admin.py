2a78745e6ae0d7e1faad5a22986315778829e6f6
pitweazle <rechen@trainer.schule>
2024-08-13 11:37:12 +0200

    Protokoll verbessert

--- duell/admin.py
+++ duell/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import  Duellant, Duell_Protokoll, Duell_Wertung
+from .models import  Duellant, Duell_Protokoll, Duell_Eingabe
 
 admin.site.register(Duellant)
 admin.site.register(Duell_Protokoll)
-admin.site.register(Duell_Wertung)
+admin.site.register(Duell_Eingabe)
