0d7ac5adec7185bbbb0c38ace9695ed9226ec1d9
pitweazle <rechen@trainer.schule>
2025-02-06 14:36:44 +0100

    stand 06.02.25

--- duell/urls.py
+++ duell/urls.py
@@ -14,8 +14,16 @@ urlpatterns = [
     path('duell_loeschen/', views.duell_loeschen, name='duell_loeschen'),
     path('duell_how_to/', views.duell_how_to, name='duell_how_to'),
     path("duell_protokoll/", views.duell_protokoll, name="duell_protokoll"),
-    
+
+    # für temporäre Gruppen innerhalb der Rechentrainer.app
     path("gruppe_temp/", views.gruppe_temp, name="gruppe_temp"),
+    path("duell/neue_gruppe", views.duell_neue_gruppe, name="duell_neue_gruppe"),
     path('temp_loeschen/<int:id>/', views.temp_loeschen, name='temp_loeschen'),
+
+    # für das Duell innerhalb von rechenduell.app
+    path("duell/", views.duell, name="duell"),
+    path("duell_light_registrieren/", views.duell_light_registrieren, name="duell_light_registrieren"),
+    path("duell_light_abmelden/", views.duell_light_abmelden, name="duell_light_abmelden"),
+    path("zum_rechentrainer/", views.zum_rechentrainer, name="zum_rechentrainer"),
 
 ]
