d8fd50018e1ccb90d4a98bbd89b807ea7122ea52
pitweazle <rechen@trainer.schule>
2025-12-29 18:14:41 +0100

    nochmal verlängert

--- accounts/models.py
+++ accounts/models.py
@@ -91,7 +91,7 @@ class Profil(models.Model):
 class Geloescht(models.Model):
     benutzername = models.CharField(max_length=25, blank=True)
     grund = models.CharField(max_length=25, null=True, blank=True)
-    text = models.CharField(max_length=500)
+    text = models.CharField(max_length=1000)
     erstellt_am = models.DateTimeField(default=timezone.now)
     class Meta:
         verbose_name = 'Gelöscht'
