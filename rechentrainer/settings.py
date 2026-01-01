dff0c26213b82e91e20069b2d2b90baf69c442f0
pitweazle <rechen@trainer.schule>
2026-01-01 23:48:05 +0100

    managmentbefehle und settings geändert

--- rechentrainer/settings.py
+++ rechentrainer/settings.py
@@ -144,7 +144,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+#    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATIC_ROOT = BASE_DIR / "html" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
